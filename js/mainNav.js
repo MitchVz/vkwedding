98ebe3434fcd33a77cd33467ff38afbbc5bf4df9
mitchellvanderzee <mvanderzee@ostusa.com>
2015-03-30 20:16:45 -0400

    Fixing modal backdrop (forcing scroll to top)

--- js/mainNav.js
+++ js/mainNav.js
@@ -3,22 +3,25 @@ if (Meteor.isClient) {
     Template.mainNav.events({
         'click #rsvpNavButton': function () {
             $('#navbarMenuButton').click();
-
+            $(window).scrollTop(0);
             goToPage(1);
         },
         'click #photoGalleryButton': function () {
             $('#navbarMenuButton').click();
-
+            $(window).scrollTop(0);
             initPhotoSwipeFromDOM('#mainGallery');
         },
         'click #foodNavButton': function () {
             $('#navbarMenuButton').click();
+            $(window).scrollTop(0);
         },
         'click #registryNavButton': function () {
             $('#navbarMenuButton').click();
+            $(window).scrollTop(0);
         },
         'click #mapNavButton': function () {
             $('#navbarMenuButton').click();
+            $(window).scrollTop(0);
         }
     });
 
